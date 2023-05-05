c4b0f3fec0505cb2e9a817cb5ec98925999425aa
Sagar Narsingani <sagarhn2411@gmail.com>
2023-05-05 10:06:37 +0530

    Edit Auth0

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Auth0Provider } from '@auth0/auth0-react';
+// import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Auth0Provider
+    {/* <Auth0Provider
       domain='dev-3pot2rspbcjp0z0d.us.auth0.com'
       clientId='DCxN2BIX70WiIS1ZWOgfSxEouQGMOJRn'
       authorizationParams={{
         redirect_uri: 'http://localhost:3000/dashboard/home'
       }}
       
-    >
+    > */}
       <App />
-    </Auth0Provider>
+    {/* </Auth0Provider> */}
   </React.StrictMode>
 );
